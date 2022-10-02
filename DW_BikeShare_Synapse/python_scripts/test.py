a50530bf3ff40756a70769fcfd3b856d985926db
Shamsiddin Mukhitov <smukhitov@gmail.com>
2022-10-01 22:03:53 -0400

    add python scripts

---
+++ DW_BikeShare_Synapse/python_scripts/test.py
@@ -0,0 +1,17 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+########################################
+# Update connection string information #
+########################################
+host = "shamspostgre.postgres.database.azure.com"
+user = "smukhitov"
+password = "Shams1989!"
+
+# Create a new DB
+sslmode = "require"
+dbname = "postgres"
+conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+conn = psycopg2.connect(conn_string)
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT);
+print("Connection established")
